c5c468d05ddce2b6e37813874b305e4b0923b28f
Angel Tormos Gual <angtorgua2@alu.edu.gva.es>
2024-01-29 09:02:47 +0100

    POO update

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    private String name;
+    private String address;
+
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+
+    public String toString(){
+        return this.name + "\n\t" + this.address;
+    }
+}
